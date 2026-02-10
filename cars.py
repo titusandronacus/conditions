4477c75fef4d208b1d5f8076ffebefbc335a7af0
Travis Burke <tburke22@cscc.edu>
2026-02-10 10:26:31 -0500

    updated files with lecture contents

---
+++ cars.py
@@ -0,0 +1,10 @@
+cars = ['audi', 'bmw', 'subaru', 'toyota']
+
+for car in cars:
+    if car == 'bmw':
+        print(car.upper())
+    else:
+        print(car.title())
+
+is_car_bmw = car == 'bmw'
+print(is_car_bmw)
---
+++ toppings.py
@@ -0,0 +1,6 @@
+requested_topping = 'mushrooms'
+
+if requested_topping != 'anchovies':
+    print('Hold the anchovies!')
+
+    
