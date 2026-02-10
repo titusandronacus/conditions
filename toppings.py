2ba2932604db0eef0bf34852257fa88183ad03b8
Travis Burke <tburke22@cscc.edu>
2026-02-10 11:23:37 -0500

    get rid of current work, we're doing something else

---
+++ toppings.py
@@ -0,0 +1,10 @@
+requested_toppings = ['mushrooms', 'extra cheese']
+
+if 'mushrooms' in requested_toppings:
+    print('adding mushrooms.')
+elif 'pepperoni' in requested_toppings:
+    print('adding pepperoni.')
+elif 'extra cheese' in requested_toppings:
+    print("adding extra cheese")
+
+print("\nFinished making your pizza!")
